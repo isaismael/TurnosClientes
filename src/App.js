90c5683a6705c8e0de3277181e6b77c336142b32
isaismael <ismael.isa02@gmail.com>
2021-10-06 12:29:31 -0300

    agregando al localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import react, {Fragment, useState} from "react";
+import react, {Fragment, useState, useEffect} from "react";
 import Formulario from "./components/Formulario";
 import Cita from "./components/Cita";
 
 function App() {
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
   //Arreglos de citas
   const[citas, guardarCitas] = useState([]);
 
@@ -13,6 +19,24 @@ function App() {
       cita
     ])
   }
+  //Use Effect para realizar ciertas operaciones
+  useEffect( () => {
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas))
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas] );
+
+  //Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter( cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  // Msj condicional
+  const titulo = citas.length === 0 ? 'No hay Citas' : 'Administra Tus Citas';
+
   return (
     <Fragment>
     <h1>Administrador de pacientes</h1>
@@ -25,11 +49,12 @@ function App() {
           />
         </div>
         <div className="one-half column" >
-          <h2>Administra tus citas</h2>
+          <h2>{titulo}</h2>
           {citas.map(cita => (
             <Cita
             key={cita.id}
             cita={cita}
+            eliminarCita={eliminarCita}
             />
           ))}
         </div>
