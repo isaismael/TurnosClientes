fa3be67751346fb26a762031fa633efbed25a8e0
isaismael <ismael.isa02@gmail.com>
2021-10-06 11:31:18 -0300

    mostrando citas del state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react, {Fragment, useState} from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
   //Arreglos de citas
@@ -24,7 +25,13 @@ function App() {
           />
         </div>
         <div className="one-half column" >
-          2
+          <h2>Administra tus citas</h2>
+          {citas.map(cita => (
+            <Cita
+            key={cita.id}
+            cita={cita}
+            />
+          ))}
         </div>
       </div>
     </div>
